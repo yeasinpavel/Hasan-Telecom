4f19c2bce2bf9d1942e911981a52931f5ba56e93
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 19:34:04 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,27 @@
 const CACHE_NAME = "ht-vault-cache-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
   "https://cdn.tailwindcss.com",
-  "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js",
-  "https://www.gstatic.com/firebasejs/12
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js"
+];
+
+self.addEventListener("install", event=>{
+  event.waitUntil(
+    caches.open(ht-vault-cache-v1).then(cache=>{
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event=>{
+  event.respondWith(
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request);
+    })
+  );
+});
