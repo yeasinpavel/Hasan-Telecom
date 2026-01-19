31c9bdf6fad29ccd7d797772b90301e495ad9a01
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 18:55:28 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -6,25 +6,5 @@ const urlsToCache = [
   "./icon-192.png",
   "./icon-512.png",
   "https://cdn.tailwindcss.com",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js"
-];
-
-// Install service worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Fetch
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/12
