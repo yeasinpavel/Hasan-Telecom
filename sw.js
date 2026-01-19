f2ddf39e1888e01cc33b849fc536e186b0b0d9b8
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 21:27:53 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = 'hasan-telecom-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
-];
+const CACHE_NAME = "hasan-telecom-v1";
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c=>c.addAll([
+      "/",
+      "/index.html",
+      "/manifest.json"
+    ]))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
 });
