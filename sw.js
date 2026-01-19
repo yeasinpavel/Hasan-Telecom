c4218f85b493ed2a0ec89d906a72b6ccdf8fa133
yeasinpavel <yeasin511436@gmail.com>
2026-01-19 20:21:27 +0600

    sw.js

--- sw.js
+++ sw.js
@@ -12,16 +12,12 @@ const urlsToCache = [
 
 self.addEventListener("install", event=>{
   event.waitUntil(
-    caches.open(ht-vault-cache-v1).then(cache=>{
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event=>{
   event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response=>response || fetch(event.request))
   );
 });
